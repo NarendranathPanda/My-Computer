a4b919d89c7b8f6db13de9064f647ade0a6ce9ef
Narendranath Panda <NarendranathPanda@users.noreply.github.com>
2020-04-09 21:44:50 +0530

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -1,12 +1,26 @@
 #! /bin/bash
 
 echo  "Select the  environment: "
-ls -X ~/.kube/
+echo "--------------------------------------------"
+
+ls -XA1 ~/.kube/k8s
+
+echo "--------------------------------------------"
+
 echo -n "Env : "
 read kube
-export KUBECONFIG=~/.kube/$kube
+export KUBECONFIG=~/.kube/k8s/$kube
 alias k=kubectl
 complete -F __start_kubectl k
 
+echo "------------------cluster-info--------------------------"
+
 k cluster-info
+
+echo "------------------context-info--------------------------"
+
 k config get-contexts
+
+echo "--------------------------------------------------------"
+echo ""
+
