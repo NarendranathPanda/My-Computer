e20a480ced8f434e19d5e53dd531de614897f429
Narendranath Panda <NarendranathPanda@users.noreply.github.com>
2020-09-13 08:33:32 +0530

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -11,6 +11,8 @@ fi
 
 kube="${kube:-config}"
 export KUBECONFIG=~/.kube/$kube
+#ref : https://stackoverflow.com/questions/50406142/kubectl-bash-completion-doesnt-work-in-ubuntu-docker-container
+source /etc/bash_completion
 alias k=kubectl
 complete -F __start_kubectl k
 source <(kubectl completion bash | sed 's/kubectl/k/g')
